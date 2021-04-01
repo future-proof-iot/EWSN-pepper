38d18f8ce889b61778b9a89c3ec285016447527d
Roudy Dagher <roudy.dagher@inria.fr>
2021-04-01 12:44:02 +0200

    Added testing base64 decode + hexdump

--- tests/pet/base64_decode.py
+++ tests/pet/base64_decode.py
@@ -1,28 +1,24 @@
 import base64
 import array
+
+def hexdump(prefix, arr):
+    hex_arr = [hex(a) for a in arr]
+    print(prefix, '{',', '.join(hex_arr),'}')
+
 pub_1 = 'wsPGv9bw2Q0mLNR8Q+TA5q4e6GQzRIiPxl5gXtrsZi8='
-pub_1_arr=list(base64.b64decode(pub_1))
-pub_1_arr_rev=[hex(a) for a in pub_1_arr]
-print(pub_1_arr_rev)
-pub_1 = 'IAB2ptLioKUQA+T4SH8AAAAAAAAAAAAAEAPk+Eh/AEA= '
-pub_1_arr=list(base64.b64decode(pub_1))
-pub_1_arr_rev=[hex(a) for a in pub_1_arr]
-print(pub_1_arr_rev)
+hexdump('pub_1 =', list(base64.b64decode(pub_1)))
+
+sec_1 = 'IAB2ptLioKUQA+T4SH8AAAAAAAAAAAAAEAPk+Eh/AEA= '
+hexdump('sec_1 =', list(base64.b64decode(sec_1)))
+
 pub_2 = 'l9OSbVr/VD2XhyCbVdvaRoIjhWxCuW1iWaQ0GeHkLzo='
-pub_2_arr=list(base64.b64decode(pub_2))
-pub_2_arr_rev=[hex(a) for a in pub_2_arr]
-print(pub_2_arr_rev)
+hexdump('pub_2 =', list(base64.b64decode(pub_2)))
+
 sec2 = 'OBgpK9a1/XWQruL4SH8AAAAAAAAAAAAAAMfgNUl/AEA='
-sec2_arr=list(base64.b64decode(sec2))
-sec2_arr_rev=[hex(a) for a in sec2_arr]
-print(sec2_arr_rev)
+hexdump('sec2 =', list(base64.b64decode(sec2)))
 
 pet_et = 'KqO9fF5bvHtJFh6uWSDBnaO4JZu6hi/AJTjLbSyPklE='
-pet_et_arr=list(base64.b64decode(pet_et))
-pet_et_arr_rev=[hex(a) for a in pet_et_arr]
-print(pet_et_arr_rev)
+hexdump('pet_et =', list(base64.b64decode(pet_et)))
 
 pet_rt = 'iscm1Ih0+xfKL38bF/jONgeGkhqSKaWyaokgxGiT+1U='
-pet_rt_arr=list(base64.b64decode(pet_rt))
-pet_rt_arr_rev=[hex(a) for a in pet_rt_arr]
-print(pet_rt_arr_rev)
+hexdump('pet_rt =', list(base64.b64decode(pet_rt)))
