3bfd6cc516a975d04c5d7c962323a02f36b8987b
Francisco Molina <femolina@uc.cl>
2021-07-05 10:04:11 +0200

    modules/sys: add epoch data cbor encoding/decoding

--- modules/sys/uwb_epoch/include/uwb_epoch.h
+++ modules/sys/uwb_epoch/include/uwb_epoch.h
@@ -29,6 +29,11 @@ extern "C" {
 #endif
 
 /**
+ * @brief UWB_EPOCH CBOR tag
+ */
+#define UWB_EPOCH_CBOR_TAG      (0xCAFE)
+
+/**
  * @brief   Step between windows in seconds
  */
 #ifndef CONFIF_EPOCH_MAX_ENOCUNTERS
@@ -49,21 +54,21 @@ typedef struct uwb_contact_data {
  * @brief   An Epoch Data Structure
  */
 typedef struct uwb_epoch_data {
-    uint32_t timestamp;                                     /**< epoch timestamp seconds*/
+    uint32_t timestamp;                                         /**< epoch timestamp seconds*/
     uwb_contact_data_t contacts[CONFIG_EPOCH_MAX_ENCOUNTERS];   /**< possible contacts */
-    crypto_manager_keys_t *keys;                            /**< keys */
+    crypto_manager_keys_t *keys;                                /**< keys */
 } uwb_epoch_data_t;
 
 /**
  * @brief   Start of an uwb_epoch
  *
- * @param[inout]    uwb_epoch       the uwb_epoch data element to initialize
+ * @param[inout]    uwb_epoch   the uwb_epoch data element to initialize
  * @param[in]       timestamp   a timestamp in seconds relative to the service
  *                              start time
- * @param[in]       keys        the crypto keys for this uwb_epoch
+ * @param[in]       keys        the crypto keys for this uwb_epoch, can be NULL
  */
 void uwb_epoch_init(uwb_epoch_data_t *uwb_epoch, uint16_t timestamp,
-                              crypto_manager_keys_t* keys);
+                    crypto_manager_keys_t *keys);
 /**
  * @brief   To be called at the end of an uwb_epoch to process all encounter data
  *
@@ -79,6 +84,63 @@ void uwb_epoch_finish(uwb_epoch_data_t *uwb_epoch, uwb_ed_list_t *list);
  * @param[in]       uwb_epoch       the uwb_epoch data to serialize
  */
 void uwb_epoch_serialize_printf(uwb_epoch_data_t *uwb_epoch);
+
+/**
+ * @brief   Returns the amount of contacts in an epoch
+ *
+ * @param[in]       uwb_epoch       the uwb_epoch data
+ */
+uint8_t uwb_epoch_contacts(uwb_epoch_data_t *epoch);
+
+/**
+ * @brief   Serialize (CBOR) uwb_epoch data
+ *
+ * @param[in]       uwb_epoch       the uwb_epoch data to serialize
+ * @param[in]       buf             pointer to allocated encoding buffer
+ * @param[in]       len             length of encoding buffer
+ *
+ * @return  Encoded length
+ */
+size_t uwb_epoch_serialize_cbor(uwb_epoch_data_t *epoch, uint8_t *buf,
+                                size_t len);
+
+/**
+ * @brief   Loads serialized uwb_epoch data (no keys)
+ *
+ * @param[in]       buf             pointer to encoded data
+ * @param[in]       len             size of the encoded data
+ * @param[in]       uwb_epoch       the uwb_epoch data to deserialize
+ *
+ * @return  0 on success, <0 otherwise
+ */
+int uwb_epoch_load_cbor(uint8_t *buf, size_t len, uwb_epoch_data_t *epoch);
+
+/**
+ * @brief   Serialize (CBOR) uwb_contact_data_t
+ *
+ * @param[in]       contact         the uwb_epoch data to serialize
+ * @param[in]       buf             pointer to allocated encoding buffer
+ * @param[in]       len             length of encoding buffer
+ *
+ * @return  Encoded length
+ */
+size_t uwb_contact_serialize_cbor(uwb_contact_data_t *contact,
+                                  uint32_t timestamp,
+                                  uint8_t *buf, size_t len);
+
+/**
+ * @brief   Loads serialized uwb_contact data
+ *
+ * @param[in]       buf             pointer to encoded data
+ * @param[in]       len             size of the encoded data
+ * @param[in]       contact         the uwb_contact_data data to deserialize
+ * @param[in]       timestamp       the timestamp for that contact
+ *
+ * @return  0 on success, <0 otherwise
+ */
+int uwb_contact_load_cbor(uint8_t *buf, size_t len, uwb_contact_data_t *contact,
+                          uint32_t *timestamp);
+
 
 #ifdef __cplusplus
 }
--- modules/sys/uwb_epoch/uwb_epoch.c
+++ modules/sys/uwb_epoch/uwb_epoch.c
@@ -27,6 +27,7 @@
 #include "fmt.h"
 #include "log.h"
 #include "test_utils/result_output.h"
+#include "nanocbor/nanocbor.h"
 
 typedef struct top_uwb_ed {
     uwb_ed_t *ed;
@@ -71,18 +72,21 @@ static int _add_to_top_list(clist_node_t *node, void *arg)
 }
 
 void uwb_epoch_init(uwb_epoch_data_t *epoch, uint16_t timestamp,
-                              crypto_manager_keys_t* keys)
+                    crypto_manager_keys_t *keys)
 {
     memset(epoch, '\0', sizeof(uwb_epoch_data_t));
     epoch->keys = keys;
     epoch->timestamp = timestamp;
-    crypto_manager_gen_keypair(keys);
+    if (keys) {
+        crypto_manager_gen_keypair(keys);
+    }
 }
 
 void uwb_epoch_finish(uwb_epoch_data_t *epoch, uwb_ed_list_t *list)
 {
     /* process all data */
     top_uwb_ed_list_t top;
+
     memset(&top, '\0', sizeof(top));
 
     /* finish list processing */
@@ -99,7 +103,8 @@ void uwb_epoch_finish(uwb_epoch_data_t *epoch, uwb_ed_list_t *list)
     }
 
     while (list->list.next) {
-        uwb_ed_memory_manager_free(list->manager, (uwb_ed_t *)clist_lpop(&list->list));
+        uwb_ed_memory_manager_free(list->manager,
+                                   (uwb_ed_t *)clist_lpop(&list->list));
     }
 }
 
@@ -111,7 +116,7 @@ static void turo_array_hex(turo_t *ctx, uint8_t *vals, size_t size)
     ctx->state = 1;
     print_str("\"");
     while (size > 0) {
-       print_byte_hex(*vals);
+        print_byte_hex(*vals);
         vals++;
         size--;
     }
@@ -123,6 +128,7 @@ void uwb_epoch_serialize_printf(uwb_epoch_data_t *epoch)
     turo_t ctx;
 
     unsigned int state = irq_disable();
+
     turo_init(&ctx);
     turo_dict_open(&ctx);
     turo_dict_key(&ctx, "epoch");
@@ -139,23 +145,137 @@ void uwb_epoch_serialize_printf(uwb_epoch_data_t *epoch)
             turo_dict_key(&ctx, "rtl");
             turo_array_hex(&ctx, epoch->contacts[i].pet.rt, PET_SIZE);
             turo_dict_close(&ctx);
-            turo_dict_close(&ctx);
-            turo_dict_open(&ctx);
             turo_dict_key(&ctx, "exposure");
             turo_u32(&ctx, epoch->contacts[i].exposure_s);
-            turo_dict_close(&ctx);
-            turo_dict_open(&ctx);
-            turo_dict_key(&ctx, "req-count");
+            turo_dict_key(&ctx, "reqcount");
             turo_u32(&ctx, epoch->contacts[i].req_count);
-            turo_dict_close(&ctx);
-            turo_dict_open(&ctx);
-            turo_dict_key(&ctx, "avg-d-cm");
+            turo_dict_key(&ctx, "avg_d_cm");
             turo_u32(&ctx, epoch->contacts[i].avg_d_cm);
             turo_dict_close(&ctx);
-       }
+        }
     }
     turo_array_close(&ctx);
     turo_dict_close(&ctx);
     print_str("\n");
     irq_restore(state);
+}
+
+uint8_t uwb_epoch_contacts(uwb_epoch_data_t *epoch)
+{
+    uint8_t contacts = 0;
+
+    for (uint8_t i = 0; i < CONFIG_EPOCH_MAX_ENCOUNTERS; i++) {
+        if (epoch->contacts[i].exposure_s != 0) {
+            contacts++;
+        }
+    }
+    return contacts;
+}
+
+size_t uwb_epoch_serialize_cbor(uwb_epoch_data_t *epoch, uint8_t *buf,
+                                size_t len)
+{
+    nanocbor_encoder_t enc;
+
+    nanocbor_encoder_init(&enc, buf, len);
+    nanocbor_fmt_tag(&enc, UWB_EPOCH_CBOR_TAG);
+    nanocbor_fmt_array(&enc, 2);
+    nanocbor_fmt_uint(&enc, epoch->timestamp);
+    uint8_t contacts = uwb_epoch_contacts(epoch);
+    nanocbor_fmt_array(&enc, contacts);
+    for (uint8_t i = 0; i < contacts; i++) {
+        nanocbor_fmt_array(&enc, 5);
+        nanocbor_put_bstr(&enc, epoch->contacts[i].pet.et, PET_SIZE);
+        nanocbor_put_bstr(&enc, epoch->contacts[i].pet.rt, PET_SIZE);
+        nanocbor_fmt_uint(&enc, epoch->contacts[i].exposure_s);
+        nanocbor_fmt_uint(&enc, epoch->contacts[i].req_count);
+        nanocbor_fmt_uint(&enc, epoch->contacts[i].avg_d_cm);
+    }
+    return nanocbor_encoded_len(&enc);
+}
+
+int uwb_epoch_load_cbor(uint8_t *buf, size_t len, uwb_epoch_data_t *epoch)
+{
+    nanocbor_value_t dec;
+    nanocbor_value_t arr1;
+    nanocbor_value_t arr2;
+    nanocbor_value_t arr3;
+    uint32_t tag;
+
+    nanocbor_decoder_init(&dec, buf, len);
+    nanocbor_get_tag(&dec, &tag);
+    if (tag != UWB_EPOCH_CBOR_TAG) {
+        return -1;
+    }
+    nanocbor_enter_array(&dec, &arr1);
+    if (!nanocbor_get_uint32(&arr1, &epoch->timestamp)) {
+        return -1;
+    }
+    nanocbor_enter_array(&arr1, &arr2);
+    for (uint8_t i = 0; i < CONFIG_EPOCH_MAX_ENCOUNTERS; i++) {
+        if (nanocbor_at_end(&arr2)) {
+            break;
+        }
+        nanocbor_enter_array(&arr2, &arr3);
+        size_t blen;
+        const uint8_t *pet;
+        nanocbor_get_bstr(&arr3, &pet, &blen);
+        memcpy(epoch->contacts[i].pet.et, pet, blen);
+        nanocbor_get_bstr(&arr3, &pet, &blen);
+        memcpy(epoch->contacts[i].pet.rt, pet, blen);
+        nanocbor_get_uint16(&arr3, &epoch->contacts[i].exposure_s);
+        nanocbor_get_uint16(&arr3, &epoch->contacts[i].req_count);
+        nanocbor_get_uint8(&arr3, &epoch->contacts[i].avg_d_cm);
+        nanocbor_leave_container(&arr2, &arr3);
+    }
+
+    nanocbor_leave_container(&arr1, &arr2);
+    nanocbor_leave_container(&dec, &arr1);
+    return 0;
+}
+
+size_t uwb_contact_serialize_cbor(uwb_contact_data_t *contact,
+                                  uint32_t timestamp,
+                                  uint8_t *buf, size_t len)
+{
+    nanocbor_encoder_t enc;
+
+    nanocbor_encoder_init(&enc, buf, len);
+    nanocbor_fmt_tag(&enc, UWB_EPOCH_CBOR_TAG);
+    nanocbor_fmt_array(&enc, 6);
+    nanocbor_fmt_uint(&enc, timestamp);
+    nanocbor_put_bstr(&enc, contact->pet.et, PET_SIZE);
+    nanocbor_put_bstr(&enc, contact->pet.rt, PET_SIZE);
+    nanocbor_fmt_uint(&enc, contact->exposure_s);
+    nanocbor_fmt_uint(&enc, contact->req_count);
+    nanocbor_fmt_uint(&enc, contact->avg_d_cm);
+    return nanocbor_encoded_len(&enc);
+}
+
+int uwb_contact_load_cbor(uint8_t *buf, size_t len,
+                          uwb_contact_data_t *contact, uint32_t *timestamp)
+{
+    nanocbor_value_t dec;
+    nanocbor_value_t arr;
+    uint32_t tag;
+
+    nanocbor_decoder_init(&dec, buf, len);
+    nanocbor_get_tag(&dec, &tag);
+    /* workaround nanocbor bug see https://github.com/bergzand/NanoCBOR/pull/55 */
+    if ((uint16_t) tag != UWB_EPOCH_CBOR_TAG) {
+        return -1;
+    }
+    nanocbor_enter_array(&dec, &arr);
+    nanocbor_get_uint32(&arr, timestamp);
+    size_t blen;
+    const uint8_t *pet;
+    nanocbor_get_bstr(&arr, &pet, &blen);
+    memcpy(contact->pet.et, pet, blen);
+    nanocbor_get_bstr(&arr, &pet, &blen);
+    memcpy(contact->pet.rt, pet, blen);
+    nanocbor_get_uint16(&arr, &contact->exposure_s);
+    nanocbor_get_uint16(&arr, &contact->req_count);
+    nanocbor_get_uint8(&arr, &contact->avg_d_cm);
+    nanocbor_leave_container(&dec, &arr);
+    return 0;
 }
