b693abdcaba3fd6e2f1c0e4e7f2b65bd2ddc4c6e
Francisco Molina <femolina@uc.cl>
2021-08-23 13:44:07 +0200

    modules/sys/crypto_manager: move hash to common

--- modules/sys/crypto_manager/common.c
+++ modules/sys/crypto_manager/common.c
@@ -22,6 +22,7 @@
 
 #include "crypto_manager.h"
 #include "kernel_defines.h"
+#include "hashes/sha256.h"
 
 #define ENABLE_DEBUG    0
 #include "debug.h"
@@ -38,3 +39,45 @@ int8_t array_a_greater_than_b(uint8_t *a, uint8_t *b)
     }
     return -1;
 }
+
+int crypto_manager_gen_pet(crypto_manager_keys_t *keys, uint8_t *pk,
+                           const uint8_t prefix, uint8_t *pet)
+{
+    assert(keys && pk && prefix && pet);
+    uint8_t secret[PET_SIZE] = {0};
+    /* clear pet */
+    memset(pet, 0, PET_SIZE);
+
+    if (crypto_manager_shared_secret(keys->sk, pk, secret)) {
+        DEBUG("[crypto_manager]: failed secret generation");
+        return -1;
+    }
+
+    /* calculate hash of prefix | secret */
+    sha256_context_t sha256;
+    sha256_init(&sha256);
+    sha256_update(&sha256, &prefix, 1);
+    sha256_update(&sha256, secret, PET_SIZE);
+    sha256_final(&sha256, pet);
+
+    return 0;
+}
+
+int crypto_manager_gen_pets(crypto_manager_keys_t *keys, uint8_t *ebid,
+                            pet_t* pet)
+{
+    assert(keys && ebid && pet);
+
+    int8_t pk_gt_ebid = array_a_greater_than_b(keys->pk, ebid);
+    if (pk_gt_ebid == -1) {
+        return -1;
+    } else if (pk_gt_ebid == 0) {
+        crypto_manager_gen_pet(keys, ebid, 0x02, pet->et);
+        crypto_manager_gen_pet(keys, ebid, 0x01, pet->rt);
+    } else {
+        crypto_manager_gen_pet(keys, ebid, 0x01, pet->et);
+        crypto_manager_gen_pet(keys, ebid, 0x02, pet->rt);
+    }
+    return 0;
+}
+
