e95f03720a0e79f2445a69945417068cc76ce777
Francisco Molina <femolina@uc.cl>
2021-11-04 15:28:26 +0100

    modules/sys/epoch: merge epoch modules
    TODO: fix CBOR serilization

--- modules/sys/epoch/epoch.c
+++ modules/sys/epoch/epoch.c
@@ -22,10 +22,12 @@
 #include <assert.h>
 
 #include "irq.h"
-#include "epoch.h"
 #include "fmt.h"
-#include "log.h"
 #include "test_utils/result_output.h"
+#include "nanocbor/nanocbor.h"
+
+#include "epoch.h"
+#include "ed.h"
 
 typedef struct top_ed {
     ed_t *ed;
@@ -38,13 +40,33 @@ typedef struct top_ed_list {
     uint8_t count;
 } top_ed_list_t;
 
-/* TODO: this is a ver bad search algorithm, improve later */
+static uint16_t ed_max_exposure_time(ed_t* ed)
+{
+    uint16_t exposure = 0;
+    uint16_t tmp_exposure = 0;
+#if IS_USED(MODULE_ED_BLE)
+    tmp_exposure = ed->ble.seen_last_s - ed->ble.seen_first_s;
+    exposure = tmp_exposure > exposure ? tmp_exposure : exposure;
+#endif
+#if IS_USED(MODULE_ED_BLE_WIN)
+    tmp_exposure = ed->ble_win.seen_last_s - ed->ble_win.seen_first_s;
+    exposure = tmp_exposure > exposure ? tmp_exposure : exposure;
+#endif
+#if IS_USED(MODULE_ED_UWB)
+    tmp_exposure = ed->uwb.seen_last_s - ed->uwb.seen_first_s;
+    exposure = tmp_exposure > exposure ? tmp_exposure : exposure;
+#endif
+    return exposure;
+}
+
 static int _add_to_top_list(clist_node_t *node, void *arg)
 {
     top_ed_list_t *list = (top_ed_list_t *)arg;
-    uint16_t duration = ed_exposure_time((ed_t *)node);
+    uint16_t duration = ed_max_exposure_time((ed_t *)node);
 
+    /* if still space in list simply insert */
     if (list->count < 8) {
+        /* update the minimum value index if needed */
         if (list->top[list->min].duration > duration) {
             list->min = list->count;
         }
@@ -53,12 +75,16 @@ static int _add_to_top_list(clist_node_t *node, void *arg)
         list->count++;
     }
     else {
+        /* if exposure is smaller than the minimum exposure in the list then
+           ignore it */
         if (duration <= list->top[list->min].duration) {
             return 0;
         }
         else {
+            /* replace the current minimum by the new encounter */
             list->top[list->min].ed = (ed_t *)node;
             list->top[list->min].duration = duration;
+            /* update the minimum exposure time */
             for (uint8_t i = 0; i < CONFIG_EPOCH_MAX_ENCOUNTERS; i++) {
                 if (list->top[i].duration < list->top[list->min].duration) {
                     list->min = i;
@@ -69,38 +95,72 @@ static int _add_to_top_list(clist_node_t *node, void *arg)
     return 0;
 }
 
-void epoch_init(epoch_data_t *epoch, uint16_t timestamp,
-                              crypto_manager_keys_t* keys)
+void epoch_init(epoch_data_t *epoch, uint32_t timestamp,
+                crypto_manager_keys_t *keys)
 {
     memset(epoch, '\0', sizeof(epoch_data_t));
     epoch->keys = keys;
     epoch->timestamp = timestamp;
-    crypto_manager_gen_keypair(keys);
+    if (keys) {
+        crypto_manager_gen_keypair(keys);
+    }
 }
 
 void epoch_finish(epoch_data_t *epoch, ed_list_t *list)
 {
     /* process all data */
     top_ed_list_t top;
+
     memset(&top, '\0', sizeof(top));
 
     /* finish list processing */
     clist_foreach(&list->list, _add_to_top_list, &top);
     for (uint8_t i = 0; i < CONFIG_EPOCH_MAX_ENCOUNTERS; i++) {
         if (top.top[i].duration != 0) {
-            epoch->contacts[i].duration = top.top[i].duration;
-            memcpy(epoch->contacts[i].wins, &top.top[i].ed->wins,
-                   sizeof(rdl_windows_t));
-            crypto_manager_gen_pets(epoch->keys,
-                                    top.top[i].ed->ebid.parts.ebid.u8,
+#if IS_USED(MODULE_ED_UWB)
+            epoch->contacts[i].uwb.exposure_s = top.top[i].ed->uwb.seen_last_s - top.top[i].ed->uwb.seen_first_s;
+            epoch->contacts[i].uwb.avg_d_cm = top.top[i].ed->uwb.cumulative_d_cm;
+            epoch->contacts[i].uwb.req_count = top.top[i].ed->uwb.req_count;
+#endif
+#if IS_USED(MODULE_ED_BLE_WIN)
+            epoch->contacts[i].ble_win.exposure_s = top.top[i].ed->ble_win.seen_last_s - top.top[i].ed->ble_win.seen_first_s;
+            memcpy(epoch->contacts[i].ble_win.wins, &top.top[i].ed->ble_win.wins, sizeof(rdl_windows_t));
+#endif
+            crypto_manager_gen_pets(epoch->keys, top.top[i].ed->ebid.parts.ebid.u8,
                                     &epoch->contacts[i].pet);
-            epoch->contacts[i].obf = top.top[i].ed->obf;
         }
     }
 
     while (list->list.next) {
         ed_memory_manager_free(list->manager, (ed_t *)clist_lpop(&list->list));
     }
+}
+
+static bool _epoch_valid_contact(contact_data_t *data)
+{
+    bool valid = false;
+#if IS_USED(MODULE_ED_BLE)
+    valid = valid || (data->ble.exposure_s != 0);
+#endif
+#if IS_USED(MODULE_ED_BLE_WIN)
+    valid = valid || (data->ble_win.exposure_s != 0);
+#endif
+#if IS_USED(MODULE_ED_UWB)
+    valid = valid || (data->uwb.exposure_s != 0);
+#endif
+    return valid;
+}
+
+uint8_t epoch_contacts(epoch_data_t *epoch)
+{
+    uint8_t contacts = 0;
+
+    for (uint8_t i = 0; i < CONFIG_EPOCH_MAX_ENCOUNTERS; i++) {
+        if (_epoch_valid_contact(&epoch->contacts[i])) {
+            contacts++;
+        }
+    }
+    return contacts;
 }
 
 static void turo_array_hex(turo_t *ctx, uint8_t *vals, size_t size)
@@ -111,18 +171,20 @@ static void turo_array_hex(turo_t *ctx, uint8_t *vals, size_t size)
     ctx->state = 1;
     print_str("\"");
     while (size > 0) {
-       print_byte_hex(*vals);
+        print_byte_hex(*vals);
         vals++;
         size--;
     }
     print_str("\"");
 }
 
-void epoch_serialize_printf(epoch_data_t *epoch)
+void contact_data_serialize_all_printf(epoch_data_t *epoch)
 {
     turo_t ctx;
 
+    /* disable irq to avoid scrambled logs */
     unsigned int state = irq_disable();
+
     turo_init(&ctx);
     turo_dict_open(&ctx);
     turo_dict_key(&ctx, "epoch");
@@ -130,7 +192,7 @@ void epoch_serialize_printf(epoch_data_t *epoch)
     turo_dict_key(&ctx, "pets");
     turo_array_open(&ctx);
     for (uint8_t i = 0; i < CONFIG_EPOCH_MAX_ENCOUNTERS; i++) {
-        if (epoch->contacts[i].duration != 0) {
+        if (_epoch_valid_contact(&epoch->contacts[i])) {
             turo_dict_open(&ctx);
             turo_dict_key(&ctx, "pet");
             turo_dict_open(&ctx);
@@ -138,28 +200,36 @@ void epoch_serialize_printf(epoch_data_t *epoch)
             turo_array_hex(&ctx, epoch->contacts[i].pet.et, PET_SIZE);
             turo_dict_key(&ctx, "rtl");
             turo_array_hex(&ctx, epoch->contacts[i].pet.rt, PET_SIZE);
-            turo_dict_close(&ctx);
-            turo_dict_close(&ctx);
+#if IS_USED(MODULE_ED_UWB)
+            turo_dict_key(&ctx, "uwb");
             turo_dict_open(&ctx);
-            turo_dict_key(&ctx, "duration");
-            turo_u32(&ctx, epoch->contacts[i].duration);
+            turo_dict_key(&ctx, "exposure");
+            turo_u32(&ctx, epoch->contacts[i].uwb.exposure_s);
+            turo_dict_key(&ctx, "req_count");
+            turo_u32(&ctx, epoch->contacts[i].uwb.req_count);
+            turo_dict_key(&ctx, "avg_d_cm");
+            turo_u32(&ctx, epoch->contacts[i].uwb.avg_d_cm);
             turo_dict_close(&ctx);
+#endif
+#if IS_USED(MODULE_ED_BLE_WIN)
+            turo_dict_key(&ctx, "ble_win");
             turo_dict_open(&ctx);
-            turo_dict_key(&ctx, "Gtx");
-            turo_u32(&ctx, CONFIG_TX_COMPENSATION_GAIN - epoch->contacts[i].obf);
-            turo_dict_close(&ctx);
-            turo_dict_open(&ctx);
-            turo_dict_key(&ctx, "windows");
+            turo_dict_key(&ctx, "exposure");
+            turo_u32(&ctx, epoch->contacts[i].ble_win.exposure_s);
+            turo_dict_key(&ctx, "wins");
             turo_array_open(&ctx);
             for (uint8_t j = 0; j < WINDOWS_PER_EPOCH; j++) {
                 turo_dict_open(&ctx);
                 turo_dict_key(&ctx, "samples");
-                turo_u32(&ctx, epoch->contacts[i].wins[j].samples);
+                turo_u32(&ctx, epoch->contacts[i].ble_win.wins[j].samples);
                 turo_dict_key(&ctx, "rssi");
-                turo_float(&ctx, epoch->contacts[i].wins[j].avg);
+                turo_float(&ctx, epoch->contacts[i].ble_win.wins[j].avg);
                 turo_dict_close(&ctx);
             }
             turo_array_close(&ctx);
+            turo_dict_close(&ctx);
+#endif
+            turo_dict_close(&ctx);
             turo_dict_close(&ctx);
         }
     }
@@ -167,4 +237,145 @@ void epoch_serialize_printf(epoch_data_t *epoch)
     turo_dict_close(&ctx);
     print_str("\n");
     irq_restore(state);
+}
+
+size_t contact_data_serialize_all_cbor(epoch_data_t *epoch, uint8_t *buf,
+                            size_t len)
+{
+    nanocbor_encoder_t enc;
+
+    nanocbor_encoder_init(&enc, buf, len);
+    nanocbor_fmt_tag(&enc, EPOCH_CBOR_TAG);
+    nanocbor_fmt_array(&enc, 2);
+    nanocbor_fmt_uint(&enc, epoch->timestamp);
+    uint8_t contacts = epoch_contacts(epoch);
+
+    nanocbor_fmt_array(&enc, contacts);
+    for (uint8_t i = 0; i < contacts; i++) {
+        nanocbor_fmt_array(&enc, 2 + 2 * IS_USED(MODULE_ED_UWB) + 2 * IS_ACTIVE(MODULE_ED_BLE_WIN));
+        nanocbor_put_bstr(&enc, epoch->contacts[i].pet.et, PET_SIZE);
+        nanocbor_put_bstr(&enc, epoch->contacts[i].pet.rt, PET_SIZE);
+#if IS_USED(MODULE_ED_UWB)
+        nanocbor_fmt_tag(&enc, ED_UWB_CBOR_TAG);
+        nanocbor_fmt_array(&enc, 3);
+        nanocbor_fmt_uint(&enc, epoch->contacts[i].uwb.exposure_s);
+        nanocbor_fmt_uint(&enc, epoch->contacts[i].uwb.req_count);
+        nanocbor_fmt_uint(&enc, epoch->contacts[i].uwb.avg_d_cm);
+#endif
+// #if IS_USED(MODULE_ED_BLE_WIN)
+//         nanocbor_fmt_tag(&enc, ED_BLE_WIN_CBOR_TAG);
+//         nanocbor_fmt_array(&enc, 2);
+//         nanocbor_fmt_uint(&enc, epoch->contacts[i].ble_win.exposure_s);
+//         nanocbor_fmt_array(&enc, WINDOWS_PER_EPOCH);
+//         for (uint8_t j = 0; j < WINDOWS_PER_EPOCH; j++) {
+//             nanocbor_fmt_uint(&enc, epoch->contacts[i].ble_win.wins[j].samples);
+//             nanocbor_fmt_uint(&enc, epoch->contacts[i].ble_win.wins[j].avg);
+//         }
+// #endif
+    }
+    return nanocbor_encoded_len(&enc);
+}
+
+int contact_data_load_all_cbor(uint8_t *buf, size_t len, epoch_data_t *epoch)
+{
+    nanocbor_value_t dec;
+    nanocbor_value_t arr1;
+    nanocbor_value_t arr2;
+    nanocbor_value_t arr3;
+    uint32_t tag;
+
+    nanocbor_decoder_init(&dec, buf, len);
+    nanocbor_get_tag(&dec, &tag);
+    if (tag != EPOCH_CBOR_TAG) {
+        return -1;
+    }
+    nanocbor_enter_array(&dec, &arr1);
+    if (!nanocbor_get_uint32(&arr1, &epoch->timestamp)) {
+        return -1;
+    }
+    nanocbor_enter_array(&arr1, &arr2);
+    for (uint8_t i = 0; i < CONFIG_EPOCH_MAX_ENCOUNTERS; i++) {
+        if (nanocbor_at_end(&arr2)) {
+            break;
+        }
+        nanocbor_enter_array(&arr2, &arr3);
+        size_t blen;
+        const uint8_t *pet;
+        nanocbor_get_bstr(&arr3, &pet, &blen);
+        memcpy(epoch->contacts[i].pet.et, pet, blen);
+        nanocbor_get_bstr(&arr3, &pet, &blen);
+        memcpy(epoch->contacts[i].pet.rt, pet, blen);
+#if IS_USED(MODULE_ED_UWB)
+        nanocbor_get_tag(&dec, &tag);
+        if (tag == ED_UWB_CBOR_TAG) {
+            nanocbor_get_uint16(&arr3, &epoch->contacts[i].uwb.exposure_s);
+            nanocbor_get_uint16(&arr3, &epoch->contacts[i].uwb.req_count);
+            nanocbor_get_uint8(&arr3, &epoch->contacts[i].uwb.avg_d_cm);
+            nanocbor_leave_container(&arr2, &arr3);
+        }
+#endif
+    }
+    nanocbor_leave_container(&arr1, &arr2);
+    nanocbor_leave_container(&dec, &arr1);
+    return 0;
+}
+
+// TODO: allow to serialize one contact at a time, similar to SenML
+// size_t epoch_serialize_cbor(epoch_data_t *contact, uint32_t timestamp,
+//                                    uint8_t *buf, size_t len);
+// {
+//     nanocbor_encoder_t enc;
+
+//     nanocbor_encoder_init(&enc, buf, len);
+//     nanocbor_fmt_tag(&enc, EPOCH_CBOR_TAG);
+//     nanocbor_fmt_array(&enc, 6);
+//     nanocbor_fmt_uint(&enc, timestamp);
+//     nanocbor_put_bstr(&enc, contact->pet.et, PET_SIZE);
+//     nanocbor_put_bstr(&enc, contact->pet.rt, PET_SIZE);
+//     nanocbor_fmt_uint(&enc, contact->exposure_s);
+//     nanocbor_fmt_uint(&enc, contact->req_count);
+//     nanocbor_fmt_uint(&enc, contact->avg_d_cm);
+//     return nanocbor_encoded_len(&enc);
+// }
+
+// int uwb_contact_load_cbor(uint8_t *buf, size_t len,
+//                           uwb_contact_data_t *contact, uint32_t *timestamp)
+// {
+//     nanocbor_value_t dec;
+//     nanocbor_value_t arr;
+//     uint32_t tag;
+
+//     nanocbor_decoder_init(&dec, buf, len);
+//     nanocbor_get_tag(&dec, &tag);
+//     nanocbor_enter_array(&dec, &arr);
+//     nanocbor_get_uint32(&arr, timestamp);
+//     size_t blen;
+//     const uint8_t *pet;
+
+//     nanocbor_get_bstr(&arr, &pet, &blen);
+//     memcpy(contact->pet.et, pet, blen);
+//     nanocbor_get_bstr(&arr, &pet, &blen);
+//     memcpy(contact->pet.rt, pet, blen);
+//     nanocbor_get_uint16(&arr, &contact->exposure_s);
+//     nanocbor_get_uint16(&arr, &contact->req_count);
+//     nanocbor_get_uint8(&arr, &contact->avg_d_cm);
+//     nanocbor_leave_container(&dec, &arr);
+//     return 0;
+// }
+
+void epoch_data_memory_manager_init(epoch_data_memory_manager_t *manager)
+{
+    memset(manager, '\0', sizeof(epoch_data_memory_manager_t));
+    memarray_init(&manager->mem, manager->buf, sizeof(epoch_data_t), CONFIG_EPOCH_DATA_BUF_SIZE);
+}
+
+void epoch_data_memory_manager_free(epoch_data_memory_manager_t *manager,
+                                    epoch_data_t *epoch_data)
+{
+    memarray_free(&manager->mem, epoch_data);
+}
+
+epoch_data_t *epoch_data_memory_manager_calloc(epoch_data_memory_manager_t *manager)
+{
+    return memarray_calloc(&manager->mem);
 }
--- modules/sys/epoch/include/epoch.h
+++ modules/sys/epoch/include/epoch.h
@@ -7,14 +7,11 @@
  */
 
 /**
- * @defgroup    sys_epoch Epoch Encounters
+ * @defgroup    sys_epoch UWB Epoch Encounters
  * @ingroup     sys
- * @brief       Desire Encounter Data Structure Per Epoch
+ * @brief       Desire Encounter Data Structure Per UWB Epoch
  *
  * @{
- *
- * TODO:
- *      - use flt16 for the rssi average, maybe libfixmath?
  *
  * @file
  *
@@ -25,49 +22,97 @@
 #define EPOCH_H
 
 #include "crypto_manager.h"
-#include "rdl_window.h"
 #include "ed.h"
+#include "memarray.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /**
+ * @brief EPOCH CBOR tag
+ * @{
+ */
+#define EPOCH_CBOR_TAG                      (0x4544)
+#define ED_UWB_CBOR_TAG                     (0x4500)
+#define ED_BLE_CBOR_TAG                     (0x4501)
+#define ED_BLE_WIN_CBOR_TAG                 (0x4502)
+/** @} */
+
+/**
  * @brief   Step between windows in seconds
  */
 #ifndef CONFIF_EPOCH_MAX_ENOCUNTERS
-#define CONFIG_EPOCH_MAX_ENCOUNTERS        8
+#define CONFIG_EPOCH_MAX_ENCOUNTERS         8
+#endif
+
+/**
+ * @brief   Size of the uwb epoch data memory buffer
+ */
+#ifndef CONFIG_EPOCH_DATA_BUF_SIZE
+#define CONFIG_EPOCH_DATA_BUF_SIZE          2
+#endif
+
+#if IS_USED(MODULE_ED_UWB)
+/**
+ * @brief   Contact data
+ */
+typedef struct contact_uwb_data {
+    uint16_t req_count;                     /**< request count */
+    uint16_t exposure_s;                    /**< exposure time */
+    uint8_t avg_d_cm;                       /**< distance */
+} contact_uwb_data_t;
+#endif
+
+#if IS_USED(MODULE_ED_BLE_WIN)
+/**
+ * @brief   Contact data
+ */
+typedef struct contact_ble_win_data {
+    rdl_window_t wins[WINDOWS_PER_EPOCH];   /**< individual windows */
+    uint16_t exposure_s;                    /**< exposure time */
+} contact_ble_win_data_t;
 #endif
 
 /**
  * @brief   Contact data
  */
 typedef struct contact_data {
-    uint16_t duration;                      /**< encounter duration */
     pet_t pet;                              /**< pet pair */
-    rdl_window_t wins[WINDOWS_PER_EPOCH];   /**< individual windows */
-    int16_t obf;                            /**< obfuscation value */
+#if IS_USED(MODULE_ED_UWB)
+    contact_uwb_data_t uwb;                 /**< UWB contact data */
+#endif
+#if IS_USED(MODULE_ED_BLE_WIN)
+    contact_ble_win_data_t ble_win;         /**< Windowed BLE contact data */
+#endif
 } contact_data_t;
 
 /**
  * @brief   An Epoch Data Structure
  */
 typedef struct epoch_data {
-    uint16_t timestamp;                                     /**< epoch timestamp */
+    uint32_t timestamp;                                     /**< epoch timestamp seconds*/
     contact_data_t contacts[CONFIG_EPOCH_MAX_ENCOUNTERS];   /**< possible contacts */
     crypto_manager_keys_t *keys;                            /**< keys */
 } epoch_data_t;
 
 /**
+ * @brief   UWB Epoch data memory manager structure
+ */
+typedef struct epoch_data_memory_manager {
+    uint8_t buf[CONFIG_EPOCH_DATA_BUF_SIZE * sizeof(epoch_data_t)]; /**< Task buffer */
+    memarray_t mem;                                                 /**< Memarray management */
+} epoch_data_memory_manager_t;
+
+/**
  * @brief   Start of an epoch
  *
- * @param[inout]    epoch       the epoch data element to initialize
+ * @param[inout]    epoch   the epoch data element to initialize
  * @param[in]       timestamp   a timestamp in seconds relative to the service
  *                              start time
- * @param[in]       keys        the crypto keys for this epoch
+ * @param[in]       keys        the crypto keys for this epoch, can be NULL
  */
-void epoch_init(epoch_data_t *epoch, uint16_t timestamp,
-                              crypto_manager_keys_t* keys);
+void epoch_init(epoch_data_t *epoch, uint32_t timestamp, crypto_manager_keys_t *keys);
 /**
  * @brief   To be called at the end of an epoch to process all encounter data
  *
@@ -78,11 +123,90 @@ void epoch_init(epoch_data_t *epoch, uint16_t timestamp,
 void epoch_finish(epoch_data_t *epoch, ed_list_t *list);
 
 /**
+ * @brief   Returns the amount of contacts in an epoch
+ *
+ * @param[in]       epoch       the epoch data
+ */
+uint8_t epoch_contacts(epoch_data_t *epoch);
+
+/**
+ * @brief   Serialize (CBOR) contact_uwb_data_t
+ *
+ * @param[in]       contact         the epoch data to serialize
+ * @param[in]       timestamp       the timestamp for that contact
+ * @param[in]       buf             pointer to allocated encoding buffer
+ * @param[in]       len             length of encoding buffer
+ *
+ * @return  Encoded length
+ */
+size_t contact_data_serialize_cbor(epoch_data_t *data, uint8_t idx, uint8_t *buf, size_t len);
+
+/**
+ * @brief   Loads serialized contact_uwb data
+ *
+ * @param[in]       buf             pointer to encoded data
+ * @param[in]       len             size of the encoded data
+ * @param[in]       contact         the contact_uwb_data data to deserialize
+ * @param[in]       timestamp       the timestamp for that contact
+ *
+ * @return  0 on success, <0 otherwise
+ */
+int contact_data_load_cbor(uint8_t *buf, size_t len, epoch_data_t *data, uint8_t idx);
+
+/**
+ * @brief   Serialize (CBOR) epoch data
+ *
+ * @param[in]       epoch       the epoch data to serialize
+ * @param[in]       buf             pointer to allocated encoding buffer
+ * @param[in]       len             length of encoding buffer
+ *
+ * @return  Encoded length
+ */
+size_t contact_data_serialize_all_cbor(epoch_data_t *epoch, uint8_t *buf, size_t len);
+
+/**
+ * @brief   Loads serialized epoch data (no keys)
+ *
+ * @param[in]       buf             pointer to encoded data
+ * @param[in]       len             size of the encoded data
+ * @param[in]       epoch           the epoch data to deserialize
+ *
+ * @return  0 on success, <0 otherwise
+ */
+int contact_data_load_all_cbor(uint8_t *buf, size_t len, epoch_data_t *epoch);
+
+/**
  * @brief   Serialize (JSON) an print epoch data
  *
  * @param[in]       epoch       the epoch data to serialize
  */
-void epoch_serialize_printf(epoch_data_t *epoch);
+void contact_data_serialize_all_printf(epoch_data_t *epoch);
+
+/**
+ * @brief   Init the memory manager
+ *
+ * @param[in]   manager     the memory manager structure to init
+ */
+void epoch_data_memory_manager_init(epoch_data_memory_manager_t *manager);
+
+/**
+ * @brief   Free an allocated element
+ *
+ * @param[in]       manager         the memory manager
+ * @param[inout]    epoch_data  the encounter data to to free
+ */
+void epoch_data_memory_manager_free(epoch_data_memory_manager_t *manager,
+                                    epoch_data_t *epoch_data);
+
+/**
+ * @brief   Allocate some space for a new encounter data entry
+ *
+ * @param[in]       manager         the memory manager
+ *
+ * @returns         pointer to the allocated encounter data structure
+ */
+epoch_data_t *epoch_data_memory_manager_calloc(epoch_data_memory_manager_t *manager);
+
 
 #ifdef __cplusplus
 }
