79a6969131d8bd908e9d069105d9ca8273bf0664
Francisco Molina <femolina@uc.cl>
2022-04-29 15:59:36 +0200

    pythonlibs: add distance function to ExperimentNode

--- dist/pythonlibs/pepper_data/experiment_data.py
+++ dist/pythonlibs/pepper_data/experiment_data.py
@@ -5,6 +5,7 @@ Dataclass definitions matchings pepper JSON formatted end of epoch logs
 """
 
 import json
+import math
 from dataclasses import dataclass, asdict
 from dacite import from_dict
 from typing import List, Optional, Tuple, Union
@@ -26,6 +27,13 @@ class ExperimentNode:
     def from_json_str(json_string: str):
         json_dict = json.loads(json_string)
         return from_dict(data_class=ExperimentNode, data=json_dict)
+
+    def distance(self, neighbor:'ExperimentNode') -> Union[float, int]:
+        return math.sqrt(
+            math.pow(self.position[0] - neighbor.position[0], 2)
+            + math.pow(self.position[1] - neighbor.position[1], 2)
+            + math.pow(self.position[2] - neighbor.position[2], 2)
+        )
 
     def to_json_str(self, indent=None) -> str:
         json_dict = asdict(self)
